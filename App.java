902e46dbd39e0afe88551b77ba472e54bae8cde0
oscarreyes1 <oscarrey92@gmail.com>
2022-05-28 16:16:18 -0600

    Fixed formatting

--- App.java
+++ App.java
@@ -1,58 +1,62 @@
 interface Logger {
-  
+
   public void Log(String text);
-  public void Error(String text); 
-  
+
+  public void Error(String text);
+
 }
+
 
 class AsteriskLogger implements Logger {
   public void Log(String text) {
     System.out.println("***" + text + "***");
   }
-  
- public void Error(String text) {
-   System.out.println("*****************");
-   System.out.println("***Error:" + text + "***");
-   System.out.println("*****************");
-   
- }
+
+  public void Error(String text) {
+    System.out.println("*****************");
+    System.out.println("***Error:" + text + "***");
+    System.out.println("*****************");
+
+  }
 }
+
 
 class SpacedLogger implements Logger {
   public void Log(String text) {
-     
-    for(int i = 0; i < text.length(); i++) {
+
+    for (int i = 0; i < text.length(); i++) {
       System.out.print(text.charAt(i) + " ");
-      
+
     }
   }
-  
+
   public void Error(String text) {
     System.out.print("ERROR:");
-    
-    for(int i = 0; i < text.length(); i++) {
+
+    for (int i = 0; i < text.length(); i++) {
       System.out.print(text.charAt(i) + " ");
-      
+
     }
-   
+
   }
 }
-  
+
+
 public class App {
 
   public static void main(String[] args) {
     // classname customname = new classname();
-AsteriskLogger asteriskLogger = new AsteriskLogger(); 
-asteriskLogger.Log("Hello");
+    AsteriskLogger asteriskLogger = new AsteriskLogger();
+    asteriskLogger.Log("Hello");
 
-asteriskLogger.Error("Hello");
+    asteriskLogger.Error("Hello");
 
-SpacedLogger spacedLogger = new SpacedLogger();
-spacedLogger.Log("Hello");
-    
-System.out.println();
+    SpacedLogger spacedLogger = new SpacedLogger();
+    spacedLogger.Log("Hello");
 
-spacedLogger.Error("Hello");
+    System.out.println();
+
+    spacedLogger.Error("Hello");
 
   }
 
